e21647b5b8deae24a72efb130551aee220e76eb3
Curtis Hamm <curt.hamm@gmail.com>
2025-04-04 16:17:43 -0600

    tf import practice

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-resource "aws_instance" "MyInstance_test" {
-    #instance configuration
+resource "aws_instance" "existing-instance" {
+    ami = "ami-0100e595e1cc1ff7f"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "existing-instance"
+    }
 }
